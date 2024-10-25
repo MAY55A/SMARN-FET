72465daa6b4203d4f49e30390f8769523fa5f5f5
sarra <sarrahhbam@gmail.com>
2024-10-25 19:20:51 +0100

    pubspec changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:smarn/pages/Home.dart';
+import 'package:smarn/pages/home.dart'; // Ensure you have this file
+import 'package:smarn/firebase_options.dart';
 
-void main() {
+//Firebase configuration
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
